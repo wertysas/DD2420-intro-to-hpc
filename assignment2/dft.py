8c2986d6cdd9dcb4bf34706629af232497a70479
wertysas <johanerics@gmail.com>
2022-03-15 10:05:29 +0100

    minor changes

--- assignment2/dft.py
+++ assignment2/dft.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 def DFT(xre, xim, Xre, Xim):
-    assert len(xre) == len(xim), "Error input dimensions does not agree"
+    #assert len(xre) == len(xim), "Error input dimensions does not agree"
     N = len(xre)
     for k in range(N):
         Xre[k] = 0
@@ -16,3 +16,28 @@ def DFT(xre, xim, Xre, Xim):
             Xre[k] += xre[n] * np.cos(k*2*np.pi*n/N) + xim[n]*np.sin(k*2*np.pi*n/N)
             Xim[k] += -xre[n] * np.sin(k*2*np.pi*n/N) + xim[n]*np.cos(k*2*np.pi*n/N)
 
+
+def DFT2(xre, xim ,Xre, Xim):
+    N = len(xre)
+    w = 2*np.pi/N
+    for k in range(N):
+        Xre[k] = 0
+        Xim[k] = 0
+        wk = w*k
+        for n in range(N):
+            Xre[k] += xre[n] * np.cos(wk*n) + xim[n]*np.sin(wk*n)
+            Xim[k] += -xre[n] * np.sin(wk*n) + xim[n]*np.cos(wk*n)
+
+
+
+if __name__ == '__main__':
+    N = 1024
+    t = np.linspace(0, 1, N)
+    s = np.random.normal(0, 0.1, N)
+    xre = [np.sin(2 * np.pi * ti) + si for ti, si in zip(t, s)]
+    xim = [np.cos(2 * np.pi * ti) + si for ti, si in zip(t, s)]
+
+    # DFT calculation
+    Xre = [0 for _ in range(N)]
+    Xim = [0 for _ in range(N)]
+    DFT2(xre, xim, Xre, Xim)
--- assignment2/dft_perf.py
+++ assignment2/dft_perf.py
@@ -5,7 +5,7 @@ Date: 2022-03-14
 
 import numpy as np
 import matplotlib.pyplot as plt
-from assignment2.dft import DFT
+from assignment2.dft import DFT, DFT2
 from time import perf_counter_ns as timer
 
 
@@ -32,19 +32,28 @@ def time_dft(n):
     t0 = timer()
     X = np.fft.fft(xnp)
     elapsed_time_np = timer() - t0
-    return elapsed_time, elapsed_time_np
+    mu = np.mean(X)
+
+    # DFT2 timing
+    t0 = timer()
+    DFT2(xre, xim , Xre, Xim)
+    dft2_time = timer() - t0
+
+    return elapsed_time, elapsed_time_np, dft2_time
 
 
-N = [8, 16, 32, 64] + [64*i for i in range(2, 16)]
+N = [8, 16, 32, 64] + [64*i for i in range(2, 14)] # should be up to 16 (64*16=1024)
 timings = []
 timings_np = []
+timings_dft2 = []
 
 for n in N:
-    t, tnp =time_dft(n)
+    t, tnp, t_dft2 =time_dft(n)
     timings.append(t)
     timings_np.append(tnp)
-
-plt.plot(N, np.log2(timings), label='DFT list implementation')
-plt.plot(N, np.log2(timings_np), label='numpy.fft.fft')
+    timings_dft2.append(t_dft2)
+plt.plot(N, timings, label='DFT list implementation')
+plt.plot(N, timings_dft2, label='DFT2 list implementation')
+plt.plot(N, timings_np, label='numpy.fft.fft')
 plt.legend()
 plt.show()
